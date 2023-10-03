6c9ae66f10f26fb22104abc53bcf208afc311d44
Ivan Drenjanin <drenjanin.i@gmail.com>
2023-10-03 18:24:59 +0200

    add: swagger and base middleware logic;

--- Makefile
+++ Makefile
@@ -11,7 +11,10 @@ clean:
 	rm -rf $(BUILD_DIR)/*
 	rm -rf *.out
 
-build: 
+swag:
+	swag init
+
+build: swag clean
 	CGO_ENABLED=0  go build -ldflags="-w -s" -o $(BUILD_DIR)/$(APP_NAME) main.go
 
 run: build
--- cmd/server/serve.go
+++ cmd/server/serve.go
@@ -8,6 +8,8 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/ivandrenjanin/go-fiber-htmx-boilerplate/pkg/config"
+	"github.com/ivandrenjanin/go-fiber-htmx-boilerplate/pkg/middleware"
+	"github.com/ivandrenjanin/go-fiber-htmx-boilerplate/pkg/route"
 	"github.com/ivandrenjanin/go-fiber-htmx-boilerplate/platform/database"
 	"github.com/ivandrenjanin/go-fiber-htmx-boilerplate/platform/logger"
 )
@@ -23,9 +25,17 @@ func Serve() {
 		logr.Panicf("failed database setup. error: %v", err)
 	}
 
-	// Define Fiber config & app.
+	// Define Fiber config & app
 	fiberCfg := config.FiberConfig()
 	app := fiber.New(fiberCfg)
+
+	// Attach Middlewares
+	middleware.FiberMiddleware(app)
+
+	// Routes
+	route.GeneralRoute(app)
+	route.SwaggerRoute(app)
+	route.NotFoundRoute(app)
 
 	// signal channel to capture system calls
 	sigCh := make(chan os.Signal, 1)
